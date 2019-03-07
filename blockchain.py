e16ae96c76fe39877c3e68f55cebfb2af3320df6
shadowant <1203355251@qq.com>
2019-03-08 01:26:16 +0800

    1

--- blockchain.py
+++ blockchain.py
@@ -8,10 +8,25 @@
 import hashlib
 import json
 from time import time
-from flask import Flask
+from flask import Flask, jsonify, request
 from uuid import uuid4
 
 class Blockchain(object):
+    """
+    block = {
+    'index': 1,
+    'timestamp': 1506057125.900785,
+    'transactions': [
+        {
+            'sender': "8527147fe1f5426f9dd545de4b27ee00",
+            'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+            'amount': 5,
+        }
+    ],
+    'proof': 324984774000,
+    'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+     }
+    """
     def __init__(self):
         self.chain = [] #存放block
         self.current_transactions = []
@@ -21,22 +36,6 @@ class Blockchain(object):
 
     def new_block(self, proof, previous_hash=None):
         # Create a new Block and add it to the chain
-        """
-        block = {
-        'index': 1,
-        'timestamp': 1506057125.900785,
-        'transactions': [
-            {
-                'sender': "8527147fe1f5426f9dd545de4b27ee00",
-                'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
-                'amount': 5,
-            }
-        ],
-        'proof': 324984774000,
-        'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
-         }
-        """
-
         """
         生成新块
         :param proof: <int> The proof given by the Proof of Work algorithm POW
@@ -122,4 +121,70 @@ class Blockchain(object):
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
-    啊
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+# 创建挖矿接口
+@app.route('/mine', methods=['GET'])
+def mine():
+    # We run the proof of work algorithm to get the next proof...
+
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # 给工作量证明的节点提供奖励.
+    # 发送者为 "0" 表明是新挖出的币
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    block = blockchain.new_block(proof)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+# 创建交易接口 可以给节点发送post交易数据
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    # 格式检查
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    # 返回的index是存储这个交易记录的区块的index
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+# 返回整个区块链
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- flaskTest.py
+++ flaskTest.py
@@ -5,11 +5,20 @@
 @time: 2019/3/6 23:40
 '''
 
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/user/<name>")
+def showName(name):
+    return name
+
+@app.route("/aa/<index>/<name>")
+def show_jsonify(index, name):
+    return jsonify({'index':index, 'name':name}), 200
 
 if __name__ == "__main__":
     app.run()
